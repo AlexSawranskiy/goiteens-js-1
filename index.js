3a3357a031566ae42644cf37dc95ed9a7524de02
AlexSawranskiy <alexsawranskiy989@gmail.com>
2022-10-26 18:16:15 +0300

    ДЗ 5

--- index.js
+++ index.js
@@ -71,17 +71,17 @@
 
 // // 3
 
-let input;
+// let input;
 
-let total = 0;
+// let total = 0;
 
-while(input !== null){
-   input = Number(prompt('Введи число'));
+// while(input !== null){
+//    input = Number(prompt('Введи число'));
 
-   total += input;
+//    total += input;
 
-   console.log(`Загальна сума чисел дорівнює ${total}`);
-}
+//    console.log(`Загальна сума чисел дорівнює ${total}`);
+// }
 
 // const number = prompt('Enter number');
 
@@ -90,3 +90,182 @@ while(input !== null){
 // }else{
 //    console.log("Число не потрапляє в діапазон");
 // }
+
+
+
+
+// 1 
+
+// 1. Створити масиви і змінні
+// const arrSum = ar1[2] + ar2[2];
+// 2. consol.log(arr1[2], arr2[2])
+
+// 2
+
+// 1. const Arr1 = [1, 5, "4", "hello"];
+// console.log(Arr1)
+// Arr1.push(22)
+// console.log(22)
+
+// 3
+
+// for (const Arr of Arr2) {
+//    console.log(typeof Arr);
+//  }
+
+// 4
+
+// 1 const message = "Welcome to Ukraine!";
+// 2 const arrMessage = message.split(' ');
+// 3 console.table(arrMessage);
+// 4 console.log(arrMessage.indexOf('l'))
+// 5 const messageStr = arrMessage.join()
+// 6 console.log(messageStr)
+
+
+// 6 
+
+// const country = prompt ('Введи назву країни');
+
+// switch (country) {
+//    case 'Китай':
+//       console.log(`Доставка в ${country} буде коштувати 100 кредитів`);
+//       break;
+
+//    default:
+//       break;
+// }
+
+
+
+
+
+
+
+
+
+
+
+// 25.10 ПРАКТИКА
+
+// 1.
+
+// const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
+
+// let total = 0;
+
+// for (const price  of cart) {
+//    total += price;
+// }
+
+// console.log(total);
+
+// 2
+// Нипиши скрипт який порахує суму всіх парних чисел в масиві.
+
+// const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 14];
+
+// let total = 0;
+
+// for (const num of numbers) {
+//    if(num % 2 === 0){
+//       console.log(number);
+//       total += number;
+//    }
+// }
+
+// console.log(total);
+
+
+// 3
+/*
+ * Напиши скрипт пошуку логіна
+ * - Якщо логіна немає, вивести повідомлення 'Користувач [логін] не знайдено.'
+ * - Якщо знайшли логін, вивести повідомлення 'Користувач [логін] знайдено.'
+ *
+ * - Спочатку через for
+ * - Потім через for...of
+ * - Логіка break
+ * - Метод includes() с тернарним оператором
+ */
+
+const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'aj4xth3m4n'];
+
+const loginToFind = 'aj4xth3m4n';
+
+for(let i = 0; i < logins.length; i++){
+    if(logins[i] === loginToFind){
+      console.log(`Користувач ${loginToFind} знайдено.`);
+    } else{
+      console.log(`Користувач не ${loginToFind} знайдено.`);
+    }
+}
+
+// або
+
+// const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'aj4xth3m4n'];
+
+// const loginToFind = 'aj4xth3m4n';
+
+// let message = `Користувач не ${loginToFind} знайдено.`
+
+// for(let i = 0; i < logins.length; i++){
+//     if(logins[i] === loginToFind){
+//       message = `Користувач ${loginToFind} знайдено.`
+//     } 
+// }
+// console.log(message);
+
+// або
+
+// const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'aj4xth3m4n'];
+
+// const loginToFind = 'aj4xth3m4n';
+
+// for (const login of logins) {
+//    if(login === loginToFind){
+//       message = `Користувач ${loginToFind} знайдено.`;
+//    }
+// }
+// console.log(message);
+
+// II
+
+// const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'aj4xth3m4n'];
+
+// const loginToFind = 'aj4xth3m4n';
+
+// if(logins.includes(loginToFind)){
+//    message = `Користувач ${loginToFind} знайдено`;
+// }
+// console.log(message);
+
+// III
+
+// const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'aj4xth3m4n'];
+
+// const loginToFind = 'aj4xth3m4n';
+
+const message = logins.includes(loginToFind) 
+? `Користувач ${loginToFind} знайдено.` 
+: `Користувач ${loginToFind} не знайдено.`;
+
+// 4
+/*
+ * Напиши скрипт який рахує суму елементів двух масивів.
+ */
+
+const array1 = [5, 10, 15, 20];
+
+const array2 = [10, 20, 30];
+
+const array3 = array1.concat(array2);
+
+let total = 0;
+
+console.log(array3);
+
+for(const num of array3){
+   total += num;
+}
+console.log(total);
